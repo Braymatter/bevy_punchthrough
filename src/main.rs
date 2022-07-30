46d584f0f50e223034ba5001e705327bfabe4c13
Cayle Bray <cayle.bray@braymatter.com>
2022-07-30 14:13:51 -0500

    fix: Plugins

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, log::LogPlugin};
 use bevy_punchthrough::server::PunchThroughServerPlugin;
 
 
 fn main(){
     let mut app = bevy::app::App::new();
-    
+
+    app.add_plugins(MinimalPlugins);
+    app.add_plugin(LogPlugin);
     app.add_plugin(PunchThroughServerPlugin);
     app.add_startup_system(server_start);
 
